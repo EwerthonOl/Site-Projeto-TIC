728fd298c1f024f81c1fbccae8f6a1493dea414d
Ewerthon Oliveira <ewerthonaraujo@outlook.com>
2023-10-17 10:48:01 -0300

    fix: scroll reveal order

--- main.js
+++ main.js
@@ -57,9 +57,9 @@ const scrollReveal = ScrollReveal({
 
 scrollReveal.reveal(
   `#home .image, #home .text,
-  #about .image, #about .text,
+  #testimonials header, #testimonials .testimonials,
   #services header, #services .card,
-  #testimonials header, #testimonials .testimonials
+  #about .image, #about .text,
   #contact .text, #contact .links,
   footer .brand, footer .social
   `,
